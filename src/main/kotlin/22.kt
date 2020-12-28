528619fab1476ace53c9a11426c60107e1e67fd0
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2020-12-28 21:26:03 +0100

    day 22

--- src/main/kotlin/22.kt
+++ src/main/kotlin/22.kt
@@ -13,6 +13,15 @@ fun main() {
             "7\n" +
             "10"
 
+    val testInput2 = "Player 1:\n" +
+            "43\n" +
+            "19\n" +
+            "\n" +
+            "Player 2:\n" +
+            "2\n" +
+            "29\n" +
+            "14"
+
     fun parseInput(lines: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
         val p1 = mutableListOf<Int>()
         val p2 = mutableListOf<Int>()
@@ -27,30 +36,55 @@ fun main() {
         return Pair(p1, p2)
     }
 
+    fun calcWinnerScore(decks: List<Int>): Long {
+        return decks.reversed().mapIndexed { i, v -> (i + 1).toLong() * v.toLong() }.sum().toLong()
+    }
+
     fun solve1(lines: List<String>): Long {
         val decks = parseInput(lines)
         while (decks.first.isNotEmpty() && decks.second.isNotEmpty()) {
             val c1 = decks.first.removeAt(0)
             val c2 = decks.second.removeAt(0)
             when {
-                c1 > c2 -> {
-                    decks.first.add(c1)
-                    decks.first.add(c2)
-                }
-                c1 < c2 -> {
-                    decks.second.add(c2)
-                    decks.second.add(c1)
-                }
-                else -> {
-                    assert(false)
-                }
+                c1 > c2 -> decks.first.addAll(listOf(c1, c2))
+                c1 < c2 -> decks.second.addAll(listOf(c2, c1))
+                else -> assert(false)
             }
         }
-        return (decks.first + decks.second).reversed().mapIndexed { i, v -> (i + 1) * v }.sum().toLong()
+        return calcWinnerScore(decks.first + decks.second)
+    }
+
+    fun playRecursiveCombat(decks: Pair<MutableList<Int>, MutableList<Int>>): Boolean {
+        val history = mutableListOf<Long>()
+        while (decks.first.isNotEmpty() && decks.second.isNotEmpty()) {
+            val currentWinnerScore = calcWinnerScore(decks.first) * calcWinnerScore(decks.second)
+            if (history.contains(currentWinnerScore)) {
+                return true
+            }
+            history.add(currentWinnerScore)
+
+            val c1 = decks.first.removeAt(0)
+            val c2 = decks.second.removeAt(0)
+            when {
+                decks.first.size >= c1 && decks.second.size >= c2 -> {
+                    if (playRecursiveCombat(Pair(decks.first.subList(0, c1).toMutableList(), decks.second.subList(0, c2).toMutableList()))) {
+                        decks.first.addAll(listOf(c1, c2))
+                    } else {
+                        decks.second.addAll(listOf(c2, c1))
+                    }
+                }
+                c1 > c2 -> decks.first.addAll(listOf(c1, c2))
+                c1 < c2 -> decks.second.addAll(listOf(c2, c1))
+                else -> assert(false)
+            }
+        }
+        return decks.second.isEmpty()
     }
 
     fun solve2(lines: List<String>): Long {
-        return 1337
+        val decks = parseInput(lines)
+        val p1Winner = playRecursiveCombat(decks)
+        return calcWinnerScore(if (p1Winner) decks.first else decks.second)
     }
 
     header(1)
@@ -58,6 +92,7 @@ fun main() {
     solve(::solve1)
 
     header(2)
-    test(::solve2, testInput, 1337)
+    test(::solve2, testInput, 291)
+    test(::solve2, testInput2, 105)
     solve(::solve2)
 }
